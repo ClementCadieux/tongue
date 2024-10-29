f05fae02554f63ddaf1cbf5f9e8bccc71a008c70
ClementCadieux <cadieuxclement@gmail.com>
2024-10-29 07:59:10 -0400

    isAtEnd impl

--- JMalt/Scanner.java
+++ JMalt/Scanner.java
@@ -8,6 +8,9 @@ import static JMalt.TokenType.*;
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
     Scanner(String source) {
         this.source = source;
@@ -22,5 +25,9 @@ public class Scanner {
 
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 }
