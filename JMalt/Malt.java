88d2c29d950d90a037ffca4db1f04b06c9373263
ClementCadieux <cadieuxclement@gmail.com>
2024-10-29 07:44:47 -0400

    run + formatting

--- JMalt/Malt.java
+++ JMalt/Malt.java
@@ -8,6 +8,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * main
@@ -40,6 +41,16 @@ public class Malt {
             if (line == null)
                 break;
             run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
         }
     }
 }
